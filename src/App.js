d36571bbf215eaf5d4732ce896b3099c201fb07c
ftolentino <filmer.tolentino@gmail.com>
2022-09-16 16:56:19 -0700

    import Canvas component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { Canvas } from '@react-three/fiber'
 import './App.css';
+
 
 function App() {
   return (
-    <div>Hello World</div>
+    <React.Fragment>
+      <div>Hello World</div>
+      <Canvas />
+    </React.Fragment>
   );
 }
 
