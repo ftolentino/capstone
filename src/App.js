9da6425a3c408e6c73580f3d5080c374af5dadfd
ftolentino <filmer.tolentino@gmail.com>
2022-09-23 10:11:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { Canvas } from '@react-three/fiber'
+import React from 'react';
+import Scene from './components/Scene';
 import './App.css';
+
+
 
 
 function App() {
   return (
     <React.Fragment>
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry args={[4, 4, 4]}/>
-          <meshStandardMaterial />
-        </mesh>
-      </Canvas>
+      <Scene />
     </React.Fragment>
   );
 }
