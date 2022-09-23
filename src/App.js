16d6aaa75b47ae7e706e534ec97fec0ec3c8403f
ftolentino <filmer.tolentino@gmail.com>
2022-09-23 13:03:11 -0700

    Setup OrbitControls and Scene

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Scene from './components/Scene';
 import './App.css';
 
+extend({OrbitControls});
 
+function Loading() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
 
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls class.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame(state => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={false}
+      maxAzimuthAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI}
+      minAzimuthAngle={-Math.PI / 4}
+      minPolarAngle={0}
+    />
+  );
+};
 
 function App() {
   return (
     <React.Fragment>
-      <Scene />
+      <div className='scene-container'>
+        <Canvas style={{ background: "white" }}>
+          <CameraControls />
+          <directionalLight intensity={0.5} />
+          <Suspense fallback={<Loading />}>
+            <Scene />
+          </Suspense>
+        </Canvas>
+      </div>
     </React.Fragment>
   );
 }
