8fb39563cbcdb178994ec555e2facfd19a8a57c7
ftolentino <filmer.tolentino@gmail.com>
2022-09-23 13:03:45 -0700

    Setup mesh

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import '../../src/App.css';
 
 const Scene = () => {
   return (
     <React.Fragment>
-      <div class="scene-container">
-        <Canvas>
-
-        </Canvas>
-      </div>
+        <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+          <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+          <meshStandardMaterial
+            attach="material"
+            color="white"
+            transparent
+            opacity={0.6}
+            roughness={1}
+            metalness={0}
+          />
+        </mesh>
     </React.Fragment>
   );
 }
