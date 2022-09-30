738d68f37b2fc4ff87cdde67b7e4c0b5d2cf2400
ftolentino <filmer.tolentino@gmail.com>
2022-09-30 14:30:54 -0700

    WIP bug fixing

--- src/App.js
+++ src/App.js
@@ -11,22 +11,6 @@ extend({OrbitControls});
 const loader = new THREE.TextureLoader()
 const arrow = loader.load('./arrow.png')
 
-function Loading() {
-  return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
-      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-      <meshStandardMaterial
-        attach="material"
-        color="white"
-        transparent
-        opacity={0.6}
-        roughness={1}
-        metalness={0}
-      />
-    </mesh>
-  );
-}
-
 // Orbit Camera Controls
 const CameraControls = () => {
   const {
@@ -58,7 +42,7 @@ function App() {
           <CameraControls />
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <Suspense fallback={<Loading />}>
+          <Suspense fallback={<h1>Loading Scene...</h1>}>
             <Sphere />
           </Suspense>
         </Canvas>
--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -4,7 +4,7 @@ const Sphere = () => {
   return (
     <React.Fragment>
         <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
-          <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+          <sphereGeometry attach="geometry" args={[1, 150, 80]} />
           <meshStandardMaterial
             attach="material"
             color="white"
