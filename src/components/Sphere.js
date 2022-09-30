b8cd70f7873ebd800bca577fc525015dfd109b7a
ftolentino <filmer.tolentino@gmail.com>
2022-09-30 15:48:43 -0700

    WIP figuring out pointmaterial

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { MathUtils } from 'three';
+import { Points, PointMaterial } from '@react-three/drei';
+
+const positions = Array.from({ length: 500 }, (i) => [
+  MathUtils.randFloatSpread(10),
+  MathUtils.randFloatSpread(10),
+  MathUtils.randFloatSpread(10),
+])
+
+
 
 const Sphere = () => {
   return (
     <React.Fragment>
         <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
           <sphereGeometry attach="geometry" args={[1, 150, 80]} />
+          <Points limit={positions.length} >
+            <PointMaterial transparent vertexColors size={15} sizeAttenuation={false} depthWrite={false} />
+          </Points>
           <meshStandardMaterial
             attach="material"
             color="white"
