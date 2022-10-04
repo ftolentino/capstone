00ed9842ca5580af0ee8abf90d297d5593713844
ftolentino <filmer.tolentino@gmail.com>
2022-10-04 09:51:46 -0700

    Add leva dependency

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { useControls } from 'leva';
 
 // Texture loader
 const loader = new THREE.TextureLoader();
