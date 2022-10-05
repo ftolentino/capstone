539868e8eb91cb5ff1b6e4c822bab74d0f9cf9ac
ftolentino <filmer.tolentino@gmail.com>
2022-10-05 09:13:29 -0700

    Commenting out dat.gui for now, it causes the whole screen to go white

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "dat.gui";
+// import * as dat from "dat.gui";
 
 // Debug
-const gui = new dat.GUI();
+// const gui = new dat.GUI();
 
 // gui.add(geometry.scale, 'x', 0, 2).name('Scale X Axis');
 
